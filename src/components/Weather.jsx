3889b111010e5532155fbd5074f37e6635c1395b
Ayushi <ayushi.b@broadwayinfotech.com>
2025-06-12 11:47:18 +0530

    u\api integration

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,6 +1,6 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import './Weather.css'
-import search from '../assets/search.png'
+import search_icon from '../assets/search.png'
 import clear from '../assets/clear.png'
 import cloud from '../assets/cloud.png'
 import drizzle from '../assets/drizzle.png'
@@ -10,44 +10,75 @@ import snow from '../assets/snow.png'
 import wind from '../assets/wind.png'
 
 const Weather = () =>{
-    const search = asycn (city) => {
+
+    const [weatherData, setWeatherData]=useState(false);
+    const inputRef = useRef();
+    const allIcons={
+        "01d":clear,
+        "02d":cloud,
+        "03d":drizzle,
+        "04d":cloud,
+        "09d":rain,
+        "10d":rain,
+        "11d":snow,
+        "13d":snow,
+        "50d":cloud,
+        "01n":clear,
+        "02n":cloud,
+        "03n":drizzle,
+        "04n":cloud,
+        "09n":rain,
+        "10n":rain,
+        "11n":snow,
+        "13n":snow,
+        "50n":cloud
+    }
+    const search = async (city) => {
         try {
-            const url=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${import.meta.env.VITE_API_KEY}`;
+            const url=`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${import.meta.env.VITE_APP_ID}`;
 
             const response = await fetch(url);
             const data = await response.json();
             console.log(data);
+            const icon = allIcons[data.weather[0].icon] || clear;
+            setWeatherData({
+                humidity: data.main.humidity,
+                wind: data.wind.speed,
+                temp: Math.floor(data.main.temp),
+                location: data.name,
+                icon:icon
+            });
 
         } catch (error) {
             
         }
     }
     useEffect(() => {
-        search('Delhi');
+        search('London');
     },[])
 
     return(
     <div className="weather">
         <h1 className="title"> Weather Forecast !</h1>
         <div className="search">
-            <input type= "text" placeholder="Enter your city"></input>
-            <img src={search}></img>
+            <input ref ={inputRef}type= "text" placeholder="Enter your city"></input>
+            <img src={search_icon} onClick={() => search(inputRef.current.value)}></img>
         </div>
-        <img src={clear} className="weather_icon"/>
-        <p className="temp">16 C</p>
-        <p className="location">Delhi</p>
+        <img src={weatherData.icon} className="weather_icon"/>
+        <p className="temp">{weatherData.temp}°C</p>
+        <p className="location">{weatherData.location}</p>
         <div className="weather-data">   
             <div className="col">   
                 <img src={humidity}></img>
                 <div>
-                    <p className="humidity">50%</p>
+                    <p className="humidity">{weatherData.humidity}</p>
                     <p>Humidity</p>
                 </div>
             </div>
             <div className="">   
                 <img src={wind}></img>
                 <div>
-                    <p className="humidity">3.6 km/hr</p>
+                    <p className="humidity">{weatherData.wind}</p>
                     <p>Wind Speed</p>
                 </div>
             </div>
